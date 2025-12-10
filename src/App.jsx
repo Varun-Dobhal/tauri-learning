d818ae2e543e78decc81d930af6fbb47f98248dc
Dobhal <varundobhal33@gmail.com>
2025-12-10 13:37:01 +0530

    Adding Clipboard Features.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Notifications from "./components/Notifications.jsx";
 import ReadWriteOpenSave from "./components/ReadWriteOpenSave.jsx";
 import TitleBar from "./components/TitleBar.jsx";
+
 function App() {
   return (
     <div>
@@ -8,6 +9,7 @@ function App() {
       <TitleBar title="Tauri App Learning" />
       {/* Read, Write, Save And Open */}
       <ReadWriteOpenSave />
+
       {/* Notification */}
       <Notifications />
     </div>
